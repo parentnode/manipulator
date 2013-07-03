9696b8cd0ea8e869950c03e264b5984f8c915afd
kaestel <martin@hvadhedderde.com>
2013-07-04 01:00:01 +0200

    ignore nodeId if debug not included

--- src/u.js
+++ src/u.js
@@ -1,10 +1,11 @@
 if(!u || !Util) {
-	var u, Util = u = new function() {}
+	var u, Util = u = new function() {};
 	u.version = 0.8;
 
 	// handle bug requests if debug lib is not included
-	u.bug = function() {}
+	u.bug = function() {};
+	u.nodeId = function() {};
 
 	// hande stats requests if stats is not enabled
-	u.stats = new function() {this.pageView = function(){};this.event = function(){};this.customVar = function(){}}
+	u.stats = new function() {this.pageView = function(){};this.event = function(){};this.customVar = function(){};}
 }
