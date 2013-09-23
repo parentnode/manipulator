cb0c92821e11a7b8a039991864335dd860b19f9b
kaestel <kaestel@e-types.com>
2013-09-23 12:22:52 +0200

    bug fixed

--- src/beta-u-scrollto.js
+++ src/beta-u-scrollto.js
@@ -1,7 +1,8 @@
 u.scrollTo = function(to, options) {
 
-
 	// scrollIn - node to do scrolling in??
+
+	u.bug_console_only = true;
 
 
 	var callback;
@@ -27,26 +28,35 @@ u.scrollTo = function(to, options) {
 
 
 	// scroll event loopback
-	window._scrollToHandler = function(event) {
+	scrollIn._scrollToHandler = function(event) {
+//		u.bug("_scrollToHandler")
 
 		u.t.resetTimer(this._current_scroll_parent.t_scroll);
 		this._current_scroll_parent.t_scroll = u.t.setTimer(this._current_scroll_parent, this._current_scroll_parent._scrollTo, 50);
-		
 	}
+
 
 	// TODO: adjust for page not being high enough for end scroll point - currently relies on #page node
 	scrollIn._to_y = u.absY(to);
+
+//	u.bug("scrollIn._to_y:" + scrollIn._to_y)
 
 	// offset y is specified
 	if(offset_y) {
 		scrollIn._to_y = scrollIn._to_y - offset_y;
 	}
 
+//	u.bug("scrollIn._to_y:" + scrollIn._to_y)
+
 	// correct for page height
 	if(scrollIn._to_y > u.qs("#page").offsetHeight-u.browserH()) {
 		scrollIn._to_y = u.qs("#page").offsetHeight-u.browserH();
 	}
 
+	// correct for negative values - cannot scroll to negative value
+	if(scrollIn._to_y < 0) {
+		scrollIn._to_y = 0;
+	}
 
 
 	scrollIn._scroll_direction = scrollIn._to_y - u.scrollY();
@@ -59,17 +69,30 @@ u.scrollTo = function(to, options) {
 //	u.bug("scrollto:" + u.scrollY() + " to " + scrollIn._to_y);
 
 	// TODO: make it possible to scroll in other elements than window
-	window._current_scroll_parent = scrollIn;
-	u.e.addEvent(window, "scroll", window._scrollToHandler);
+	scrollIn._current_scroll_parent = scrollIn;
+	u.e.addEvent(scrollIn, "scroll", scrollIn._scrollToHandler);
+//	u.bug("add scroll event:" + scrollIn._scrollToHandler.toString().substring(0, 40).replace(/\t|\n/g, ""));
+
+	scrollIn._cancelScrollTo = function() {
+
+		u.t.resetTimer(this.t_scroll);
+		u.e.removeEvent(this, "scroll", this._scrollToHandler);
+//		u.bug("remove scroll event:" + this + ", " + this._scrollToHandler.toString().substring(0, 40).replace(/\t|\n/g, ""));
+
+		this._scrollTo = null;
+
+	}
+
 
 	scrollIn._scrollTo = function(start) {
 
+//		u.bug("this:" + this)
 		// first loop - do we need to do anything special on first loop?
 		if(start) {
 		
 		}
 
-
+		
 		if(this._scroll_to_y === false || u.scrollY() == this._scroll_to_y) {
 
 			if(this._scroll_direction > 0 && (this._scroll_to_y === false || this._to_y > u.scrollY())) {
@@ -83,12 +106,17 @@ u.scrollTo = function(to, options) {
 			}
 			// done
 			else {
+//				u.bug("done")
 
-				u.t.resetTimer(this.t_scroll);
-				u.e.removeEvent(window, "scroll", window._scrollToHandler);
-				window.scrollTo(0, this._to_y);
+				this._cancelScrollTo();
 
-				window._scroll_to_y = false;
+				// u.t.resetTimer(this.t_scroll);
+				// u.e.removeEvent(window, "scroll", window._scrollToHandler);
+				// u.bug("remove scroll event:" + window._scrollToHandler.toString().substring(0, 40).replace(/\t|\n/g, ""));
+
+				this.scrollTo(0, this._to_y);
+
+				this._scroll_to_y = false;
 
 				// callback
 				if(typeof(this.scrolledTo) == "function") {
@@ -99,7 +127,7 @@ u.scrollTo = function(to, options) {
 
 			}
 
-			window.scrollTo(0, this._scroll_to_y);
+			this.scrollTo(0, this._scroll_to_y);
 
 		}
 
@@ -108,14 +136,17 @@ u.scrollTo = function(to, options) {
 		else {
 //			u.bug("cancel autoscroll");
 
-			u.t.resetTimer(this.t_scroll);
-			u.e.removeEvent(window, "scroll", window._scrollToHandler);
+			this._cancelScrollTo();
+
+			// u.t.resetTimer(this.t_scroll);
+			// u.e.removeEvent(window, "scroll", window._scrollToHandler);
+			// u.bug("remove scroll event:" + window._scrollToHandler.toString().substring(0, 40).replace(/\t|\n/g, ""));
 
 			window._scroll_to_y = false;
 
 			// callback
-			if(typeof(this.scrolledTo) == "function") {
-				this.scrolledTo();
+			if(typeof(this.scrolledToCancelled) == "function") {
+				this.scrolledToCancelled();
 			}
 		}	
 	}
