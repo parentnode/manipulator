fa01a50e7fa1303fb6d19680b068c238fa273397
kaestel <kaestel@e-types.com>
2014-11-09 12:44:00 +0000

    Reworked keyboard shortcuts and tested

--- src/u-keyboard.js
+++ src/u-keyboard.js
@@ -1,8 +1,11 @@
-// Keyboard event handler object
+// Keyboard shortcut event handler object
+// maps keys to nodes
+// default callback is node.clicked
+// metakey/ctrlkey is required as default unless it is ESC-key
 Util.Keyboard = u.k = new function() {
 
-	// Array for shortcuts
-	this.shortcuts = new Array();
+	// Object for shortcuts
+	this.shortcuts = {};
 
 	// forwarding function (onkeydown event happens on window object)
 	this.onkeydownCatcher = function(event) {
@@ -11,23 +14,8 @@ Util.Keyboard = u.k = new function() {
 	}
 
 
-// TODO: refine function syntax
-
-// u.k.addKey(node, key, _options) ?
-// where options are
-// - node
-// - function ?
-// - callback ? (string with function name or full function)
-// - what is eval intended for currently??
-
-// we should always have a node reference to check whether node still exists when shortcut is invoked
-// theory: if node does not exist, pop it and try previous
-//  - this allows for a stack of actions which should work on ajax sites
-
-
 	// default callback: clicked
-
-	// add new shortcut
+	// add new shortcut to node
 	this.addKey = function(node, key, _options) {
 
 		node.callback_keyboard = "clicked";
@@ -65,102 +53,68 @@ Util.Keyboard = u.k = new function() {
 	// callback to node.callback_keyboard (default clicked)
 	this.catchKey = function(event) {
 
-		var nodes, node, i, key;
-
 		// correct old IE event
 		event = event ? event : window.event;
 
 		// get key value
-		key = String.fromCharCode(event.keyCode);
+		var key = String.fromCharCode(event.keyCode);
 		// special case for ESC key
 		if(event.keyCode == 27) {
 			key = "ESC";
 		}
 
 //		u.bug("catchKey:" + key + ":"+event.keyCode+":" + this.shortcuts.length)
-		u.xInObject(this.shortcuts);
+//		u.xInObject(this.shortcuts);
 
 
 		// is anything registered for this key
 		if(this.shortcuts[key]) {
 
+			var nodes, node, i;
+
 			// get related nodes
 			nodes = this.shortcuts[key];
 
+			// loop throgh nodes
 			for(i = 0; node = nodes[i]; i++) {
 
-				// is node still defined
-				if(node) {
+				// is node still attached to document.body
+				if(u.nodeWithin(node, document.body)) {
 
 					// only execute visible nodes and check for metakey
-					if(node.offsetHeight && ((event.ctrlKey || event.metaKey) || !node.metakey_required)) {
+					if(node.offsetHeight && ((event.ctrlKey || event.metaKey) || (!node.metakey_required || key == "ESC"))) {
 						u.e.kill(event);
 
 						// execute
 						if(typeof(node[node.callback_keyboard]) == "function") {
 							node[node.callback_keyboard](event);
 						}
+
 					}
-					
+
 				}
-				// on ajax sites shortcuts array may live for a long time - keep it trimmed
+
 				// node is lost, remove from shortcuts array
+				// on ajax sites shortcuts object may live for a long time - keep it trimmed
+				// node is not collected by garbage collector as long as it is referenced
 				else {
+
+					// remove from array
 					this.shortcuts[key].splice(i, 1);
-					i--;
+
+					// remove key index if no nodes exist for key
+					if(!this.shortcuts[key].length) {
+						delete this.shortcuts[key];
+						break;
+					}
+					else {
+						i--;
+					}
 				}
+
 			}
 		}
 
-		u.xInObject(this.shortcuts);
-
-
-// 		if((event.ctrlKey || event.metaKey) || !node.metakey_required) {
-// 			u.e.kill(event);
-// //			action = this.shortcuts[key].pop();
-// //			for(i = 0; action = this.shortcuts[key][i]; i++) {
-// //				u.bug(key+":"+action + "::" + action.parentNode);
-// 				if(typeof(action) == "object") {
-// 					action.clicked();
-// 				}
-// 				else if(typeof(action) == "function") {
-// 					action();
-// 				}
-// 				else {
-// 					eval(action);
-// 				}
-// //			}
-//
-// 		}
-// 		if(event.keyCode == 27 && this.shortcuts["ESC"]) {
-// 			u.e.kill(event);
-//
-// 			action = this.shortcuts["ESC"].pop();
-// //			for(i = 0; action = this.shortcuts["ESC"][i]; i++) {
-// 				u.bug("esc:"+action + "::" + u.nodeId(action) + ", " + typeof(action));
-// 				if(typeof(action) == "object") {
-// 					action.clicked();
-// 				}
-// 				else if(typeof(action) == "function") {
-// 					action();
-// 				}
-// 				else {
-// 					eval(action);
-// 				}
-// //			}
-// 		}
-//		alert("Key: " + pressed_key + "\nKeyCode: " + event.keyCode + "\nCtrl:"  + event.ctrlKey + "\nMeta:"  + event.metaKey);
-
-		// if ESC -> start shortcut time window
-//		if(event.keyCode == 27) {
-//			this.timer = setTimeout('Util.Onkeydown.stopSCWindow()', 2000);
-//		}
-		// if timewindow is open, react to input
-//		else if(this.timer) {
-//			this.input += pressed_key;
-//		}
-//		this.execute(event);
 	}
-}
 
-//Util.Onkeydown.addShortcut("s", "alert('test')");
+}
