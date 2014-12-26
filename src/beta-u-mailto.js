0d81213c88530b674e8a775af94d953a8b559732
kaestel <kaestel@e-types.com>
2014-12-26 11:17:52 +0300

    Obsolete

--- src/beta-u-mailto.js
+++
@@ -1,6 +0,0 @@
-Util.otliam = function(name, dom){
-	document.write('<a onclick="Util.otliamNoise(\''+name+'\', \''+dom+'\')">'+name+'<span>@</span>'+dom+'</a>');
-}
-Util.otliamNoise = function(name, dom){
-	location.href = "ma"+"ilto:"+name+"@"+dom;
-}
