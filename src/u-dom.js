19f7ee595d4a9de85c98cd357673d32f994c2c39
kaestel <kaestel@e-types.com>
2014-12-28 14:51:37 +0300

    Added exception

--- src/u-dom.js
+++ src/u-dom.js
@@ -317,7 +317,12 @@ Util.wrapContent = u.wc = function(node, node_type, attributes) {
 // get node textcontent shorthand 
 // basically this is not needed for newer browsers, but required to align syntax for older browsers
 Util.textContent = u.text = function(node) {
-	return node.textContent;
+	try {
+		return node.textContent;
+	}
+	catch(exception) {
+		u.exception("u.text", arguments, exception);
+	}
 }
 
 
