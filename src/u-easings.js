d664b1cc679c9fa63fd5a507ad74e6f7226d2f91
kaestel <martin@think.dk>
2015-05-06 15:06:34 +0200

    Added easing collection

---
+++ src/u-easings.js
@@ -0,0 +1,21 @@
+u.easings = new function() {
+
+	this["ease-in"] = function(progress) {
+		return Math.pow((progress*this.duration) / this.duration, 3);
+	}
+
+	this["ease-out"] = function(progress) {
+		return 1 - Math.pow(1 - ((progress*this.duration) / this.duration), 3);
+	}
+
+	this["linear"] = function(progress) {
+		return (progress*this.duration) / this.duration;
+	}
+
+	this["ease-in-out"] = function(progress) {
+		if((progress*this.duration) > (this.duration / 2)) {
+			return 1 - Math.pow(1 - ((progress*this.duration) / this.duration), 3);
+		}
+		return Math.pow((progress*this.duration) / this.duration, 3);
+	}
+}
