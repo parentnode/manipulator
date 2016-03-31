3cd23f69c380662d36016bae29fbd81410e6586c
kaestel <martin@think.dk>
2016-03-31 23:49:10 +0200

    Getting closer to next version

--- src/u.js
+++ src/u.js
@@ -1,11 +1,11 @@
 if(!u || !Util) {
 	var u, Util = u = new function() {};
-	u.version = "0.9.1";
+	u.version = "0.9.2";
 
 	// handle bug requests if debug module is not included
 	u.bug = u.nodeId = u.exception = function() {};
 //	u.nodeId = function() {};
 
 	// hande stats requests if stats is not enabled
-	u.stats = new function() {this.pageView = function(){};this.event = function(){};this.customVar = function(){};}
+	u.stats = new function() {this.pageView = function(){};this.event = function(){};}
 }
