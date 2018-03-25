ed5240340cc54264f2cd5b23389573962d326830
Martin Kæstel Nielsen <martin@think.dk>
2018-03-25 15:18:15 +0200

    Add target and download option

--- src/beta-u-form-onebuttonform.js
+++ src/beta-u-form-onebuttonform.js
@@ -1,6 +1,6 @@
 Util.Objects["oneButtonForm"] = new function() {
 	this.init = function(node) {
-		// u.bug("oneButtonForm:" + u.nodeId(node));
+//		u.bug("oneButtonForm:" + u.nodeId(node));
 
 		// inject standard form if action node is empty
 		// this is done to minimize HTML in list pages
@@ -8,6 +8,7 @@ Util.Objects["oneButtonForm"] = new function() {
 
 			var csrf_token = node.getAttribute("data-csrf-token");
 			var form_action = node.getAttribute("data-form-action");
+			var form_target = node.getAttribute("data-form-target");
 			var button_value = node.getAttribute("data-button-value");
 
 			// optional attributes
@@ -17,15 +18,21 @@ Util.Objects["oneButtonForm"] = new function() {
 
 			if(csrf_token && form_action && button_value) {
 
-				node.form = u.f.addForm(node, {"action":form_action, "class":"confirm_action_form"});
+				var form_options = {"action":form_action, "class":"confirm_action_form"};
+				if(form_target) {
+					form_options["target"] = form_target;
+				}
+
+				node.form = u.f.addForm(node, form_options);
 				node.form.node = node;
 				// add csrf token
 				u.ae(node.form, "input", {"type":"hidden","name":"csrf-token", "value":csrf_token});
 
 				// add additional hidden inputs
 				if(inputs) {
-					for(input_name in inputs)
-					u.ae(node.form, "input", {"type":"hidden","name":input_name, "value":inputs[input_name]});
+					for(input_name in inputs) {
+						u.ae(node.form, "input", {"type":"hidden","name":input_name, "value":inputs[input_name]});
+					}
 				}
 
 				// add action
@@ -53,6 +60,7 @@ Util.Objects["oneButtonForm"] = new function() {
 			node.form.success_location = node.getAttribute("data-success-location");
 
 			node.form.dom_submit = node.getAttribute("data-dom-submit");
+			node.form._download = node.getAttribute("data-download");
 
 //				node.form.cancel_url = bn_cancel.href;
 
@@ -64,27 +72,42 @@ Util.Objects["oneButtonForm"] = new function() {
 			}
 
 			node.form.submitted = function() {
-				u.bug("submitted")
+				u.bug("submitted");
 
 				// first click
 				if(!u.hc(this.confirm_submit_button, "confirm") && this.confirm_submit_button.confirm_value) {
+
 					u.ac(this.confirm_submit_button, "confirm");
 					this.confirm_submit_button.value = this.confirm_submit_button.confirm_value;
 					this.t_confirm = u.t.setTimer(this, this.restore, 3000);
+
 				}
 				// confirm click
 				else {
 					u.t.resetTimer(this.t_confirm);
 
+					// Make callback to oneButtonForm node
+					if(typeof(this.node.submitted) == "function") {
+						u.bug("oneButtonForm");
+						this.node.submitted();
+					}
 
 					this.response = function(response) {
+
+						// console.log("RESPONSE");
+						// console.log(response);
 
 						u.rc(this, "submitting");
 						u.rc(this.confirm_submit_button, "disabled");
 
+						// show notification
 						page.notify(response);
 
+						// Restore button
+						this.restore();
+
 						if(response.cms_status == "success") {
+
 							// check for constraint error preventing row from actually being deleted
 							if(response.cms_object && response.cms_object.constraint_error) {
 								this.confirm_submit_button.value = this.confirm_submit_button.org_value;
@@ -93,7 +116,7 @@ Util.Objects["oneButtonForm"] = new function() {
 							else {
 
 								if(this.success_location) {
-									u.bug("location:" + this.success_location)
+									u.bug("location:" + this.success_location);
 
 									u.ass(this.confirm_submit_button, {
 										"display": "none"
@@ -102,13 +125,14 @@ Util.Objects["oneButtonForm"] = new function() {
 									location.href = this.success_location;
 								}
 								else if(this.success_function) {
-//									u.bug("function:" + this.success_function + "," + typeof(this.node[this.success_function]))
+									u.bug("function:" + this.success_function);
 									if(typeof(this.node[this.success_function]) == "function") {
 										this.node[this.success_function](response);
 									}
 								}
 								// does default callback exist
 								else if(typeof(this.node.confirmed) == "function") {
+									u.bug("confirmed");
 									this.node.confirmed(response);
 								}
 								else {
@@ -120,8 +144,6 @@ Util.Objects["oneButtonForm"] = new function() {
 							}
 						}
 
-						this.restore();
-
 					}
 					u.ac(this.confirm_submit_button, "disabled");
 
@@ -130,6 +152,21 @@ Util.Objects["oneButtonForm"] = new function() {
 
 					// regular dom submit
 					if(this.dom_submit) {
+						u.bug("should submit:" + this._download);
+						if(this._download) {
+
+							this.response({"cms_status":"success"});
+
+							// this.event_id = u.e.addWindowEvent(this, "blur", function() {
+							//
+							// 	this.response({"cms_status":"success"});
+							// 	u.e.removeWindowEvent(this, "blur", this.event_id);
+							// 	console.log("window blurred");
+							// });
+							
+
+							u.bug("wait for download");
+						}
 						this.DOMsubmit();
 					}
 					else {
