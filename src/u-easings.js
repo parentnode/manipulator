d8ef801821647632f1f0ffede28b4537f4c610f5
Martin Kæstel Nielsen <martin@think.dk>
2018-09-19 14:41:30 +0200

    Easing experiments

--- src/u-easings.js
+++ src/u-easings.js
@@ -1,7 +1,7 @@
 u.easings = new function() {
 
 	this["ease-in"] = function(progress) {
-		return Math.pow((progress*this.duration) / this.duration, 3);
+		return Math.pow((progress), 3);
 	}
 
 	this["linear"] = function(progress) {
@@ -9,17 +9,36 @@ u.easings = new function() {
 	}
 
 	this["ease-out"] = function(progress) {
-		return 1 - Math.pow(1 - ((progress*this.duration) / this.duration), 3);
+		return 1 - Math.pow(1 - ((progress)), 3);
 	}
 
 	this["linear"] = function(progress) {
-		return (progress*this.duration) / this.duration;
+		return (progress);
 	}
 
-	this["ease-in-out"] = function(progress) {
-		if((progress*this.duration) > (this.duration / 2)) {
-			return 1 - Math.pow(1 - ((progress*this.duration) / this.duration), 3);
+	this["ease-in-out-veryslow"] = function(progress) {
+		if(progress > 0.5) {
+			return 4*Math.pow((progress-1),3)+1;
 		}
-		return Math.pow((progress*this.duration) / this.duration, 3);
+		return 4*Math.pow(progress,3);  
+	}
+	this["ease-in-out"] = function(progress) {
+		if(progress > 0.5) {
+			return 1 - Math.pow(1 - ((progress)), 2);
+		}
+		return Math.pow((progress), 2);
+	}
+	this["ease-out-slow"] = function(progress) {
+		return 1 - Math.pow(1 - ((progress)), 2);
+	}
+	
+	this["ease-in-slow"] = function(progress) {
+		return Math.pow((progress), 2);
+	}
+	this["ease-in-veryslow"] = function(progress) {
+		return Math.pow((progress), 1.5);
+	}
+	this["ease-in-fast"] = function(progress) {
+		return Math.pow((progress), 4);
 	}
 }
