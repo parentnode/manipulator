867586d91933fb104d5bb8b42cf37fc59c7ccb17
Martin Kæstel Nielsen <martin@think.dk>
2019-07-18 21:28:41 +0200

    Improved context support and decoding

--- src/u-url.js
+++ src/u-url.js
@@ -2,11 +2,12 @@
 Util.getVar = function(param, url) {
 	var string = url ? url.split("#")[0] : location.search;
 
-	// find param
-	var regexp = new RegExp("[\&\?\b]{1}"+param+"\=([^\&\b]+)");
+	// find param (escape special chars in param name to make regex work)
+	var regexp = new RegExp("(?:^|\b|&|\\?)"+param.replace(/[\[\]\(\)]{1}/g, "\\$&")+"\=([^\&\b]+)");
+
 	var match = string.match(regexp);
 	if(match && match.length > 1) {
-		return match[1];
+		return decodeURIComponent(match[1]);
 	}
 	else {
 		return "";
