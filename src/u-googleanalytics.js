8895aa9828c3e313992ccc2aa7995999ff7e4628
kaestel <martin@think.dk>
2021-12-23 11:19:59 +0100

    Remove debug

--- src/u-googleanalytics.js
+++ src/u-googleanalytics.js
@@ -1,7 +1,7 @@
 u.includeGoogleAnalytics = function() {
 	if(typeof(ga) !== "function") {
 
-		u.bug("includeGoogleAnalytics");
+		// u.bug("includeGoogleAnalytics");
 
 	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
