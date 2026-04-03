fcc162627681b49742b32f3074595d5e96ade6f4
kaestel <martin@think.dk>
2026-04-03 21:04:53 +0200

    ArrayKeyValue method

--- src/u-object.js
+++ src/u-object.js
@@ -9,3 +9,23 @@ u.objectValues = function(obj) {
 	}
 	return values;
 }
+
+// Objects in array finder
+// Find index in array that has an object with specific value for key
+// Implementation of Janitor PHP variant
+u.arrayKeyValue = function(array, key, value) {
+	// u.bug("arrayKeyValue", array, key, value);
+
+	var i, object;
+	if(array && array.length) {
+
+		for(i = 0; i < array.length; i++) {
+			object = array[i];
+			if(obj(object) && object[key] === value) {
+				return i
+			}
+		}
+	}
+	return false;
+
+}
